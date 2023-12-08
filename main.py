4709de4179f83c32d282a08d33f880abc43b499d
Robin Johansson <robin.frome@gmail.com>
2023-12-08 17:03:24 +0100

    Confs is now config and reworked main.py

--- main.py
+++ main.py
@@ -1,97 +1,65 @@
-import time
 import datetime
-import terrible_term
-import confs
+from vlan import prompt_for_vlan
+from configs import prompt_for_config, generate_vlan_conf
 
-
-
-serial_port = input('Serial Ports [COM1]: ')
-
-if '' == serial_port:
-    serial_port = 'COM1'
+VERSION = '0.0.0-super-duper-beta-alpha'
+YES: tuple[str] = ('yes', 'ye', 'y', 'ja', 'japp', 'fosho')
+NO:  tuple[str] = ('no', 'n', 'nope', 'nej', 'stop', 'exit', '#metoo')
 
 print('Enter number of devices')
 num_routers = ''
-num_swiches = ''
-tot_devices = 0
+num_switches = ''
 
 while not num_routers.isnumeric():
     num_routers = input('How many Routers [0]: ')
     if '' == num_routers:
         num_routers = '0'
 
-while not num_swiches.isnumeric():
-    num_swiches = input('How many Swiches [0]: ')
-    if '' == num_swiches:
-        num_swiches = '0'
+while not num_switches.isnumeric():
+    num_switches = input('How many Swiches [0]: ')
+    if '' == num_switches:
+        num_switches = '0'
 
 num_routers = int(num_routers)
-num_swiches = int(num_swiches)
-tot_devices = num_routers + num_swiches
+num_switches = int(num_switches)
+tot_devices = num_routers + num_switches
 
 if tot_devices < 1:
     print('No devices to conf.')
     print('Exiting script')
     exit()
 
-while tot_devices > 0:
+vlans = None
+if num_switches > 0:
+    print('Do you want to add VLANs to the switches?')
+    add_vlans = input('Yes/no [no]> ')
+    if add_vlans in YES:
+        vlans = generate_vlan_conf(prompt_for_vlan())
 
-    r_hostname = 1
-    s_hostname = 1
+
+config = None
+while config is None:
+    config = prompt_for_config()
+
+# Config Routers
+host_number = 1
+while num_routers > 0:
+
+    hostname = f'R{host_number}'
+    dt = datetime.datetime.now().strftime('%H:%M:%S %d %h %Y')
+
+    host_number += 1
+    num_routers -= 1
+
+# Config Swiches
+host_number = 1
+while num_switches > 0:
+
+    hostname = f'S{host_number}'
+    dt = datetime.datetime.now().strftime('%H:%M:%S %d %h %Y')
     
-    while num_routers > 0:
-        hostname = 'R' + str(r_hostname)
-        dt = datetime.datetime.now().strftime('%H:%M:%S %d %h %Y')
+    if vlans is not None:
+        print('CONF VLAN')
 
-        print('Select config')
-        print('1: Slim Base Conf')
-        print('2: Base Conf')
-        print('3: Base Conf with SSH')
-        conf = int(input('Conf [1]: '))
-        if conf == 1 or conf == 0:
-            config =  confs.slim_base_config(hostname, dt) 
-        elif conf == 2:
-            config =  confs.base_config(hostname, dt) 
-        elif conf == 3:
-            config =  confs.ssh_base_config(hostname, dt) 
-        
-        print(f'Connect {hostname} to serial port, then press enter')
-        input()
-
-        terrible_term.send_commands(serial_port, config)
-        
-        r_hostname += 1
-        num_routers -= 1
-
-    while num_swiches > 0:
-        hostname = 'S' + str(s_hostname)
-        dt = datetime.datetime.now().strftime('%H:%M:%S %d %h %Y')
-
-        print('Select config')
-        print('1: Slim Base Conf')
-        print('2: Base Conf')
-        print('3: Base Conf with SSH')
-        conf = int(input('Conf [1]: '))
-        if conf == 1 or conf == 0:
-            config =  confs.slim_base_config(hostname, dt) 
-        elif conf == 2:
-            config =  confs.base_config(hostname, dt) 
-        elif conf == 3:
-            config =  confs.ssh_base_config(hostname, dt) 
-        
-        print(f'Connect {hostname} to serial port, then press enter')
-        input()
-
-        terrible_term.send_commands(serial_port, config)
-        
-        s_hostname += 1
-        num_swiches -= 1
-
-    tot_devices -= 1
-#
-print('#############################')
-print('User PW: cisco')
-print('Enable PW: class')
-print('SSH User: admin')
-print('SSH Pass: class')
-print('#############################')
+    host_number += 1
+    num_switches -= 1
